4729b62a85cb9597462ffbcb0401be66285de41e
LiviaTrindade <127799847+LiviaTrindade@users.noreply.github.com>
2024-05-03 19:56:49 -0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -41,24 +41,27 @@ onMounted(async () => {
 
 
 <style scoped>
+
 .character-container {
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
+  background-color: #181818;
 }
 
 .character-card {
   width: 200px;
   padding: 10px;
   border-radius: 8px;
+  margin-top: 10px;
   margin-left: 45px;
   box-shadow: 0 4px 6px rgba(0, 183, 255);
-  background-color: #fff;
+  background-color: #11c7ff;
   transition: box-shadow 0.3s ease;
 }
 
 .character-card:hover {
-  box-shadow: 0 8px 12px rgb(0, 183, 255);
+  box-shadow: 0 8px 12px rgb(0, 255, 21);
 }
 
 .character-image {
@@ -69,6 +72,7 @@ onMounted(async () => {
 .character-info {
   margin-top: 10px;
   font-size: 14px;
+  font-family: 'Comic Sans MS', cursive, sans-serif;
 }
 
 .character-info p {
